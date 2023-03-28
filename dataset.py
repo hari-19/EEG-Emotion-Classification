e1801e7226388f4f58c90a846b1c7e6ef5b14db1
Hari Hara Sudhan S <hari19@outlook.in>
2023-03-28 21:50:13 +0530

    Successfully replicated TF domain Source

---
+++ dataset.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io as sio
+from scipy import signal
+from rich.pretty import pprint
+import mne
+from sklearn.preprocessing import MinMaxScaler
+from torcheeg.datasets import DREAMERDataset
+from torcheeg.datasets.constants.emotion_recognition.dreamer import DREAMER_CHANNEL_LOCATION_DICT
+from torcheeg import transforms
+from torcheeg.utils import plot_signal
+
+dataset_path = "./DREAMER.mat"
+base_path = "./"
+
+dataset = DREAMERDataset(
+    io_path=base_path + 'dreamer8sec_unnormalized',
+    mat_path=dataset_path,
+    offline_transform=transforms.Compose([
+        transforms.BaselineRemoval(),
+        transforms.MeanStdNormalize(),
+        transforms.To2d()
+    ]),
+    # online_transform=transforms.ToTensor(),
+    label_transform=transforms.Compose(
+        [transforms.Select('valence'),
+         transforms.Binary(3.0)]),
+    chunk_size=976,
+    baseline_chunk_size=976,
+    num_baseline=8
+)
